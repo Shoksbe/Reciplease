056c2bf35377a14d2f851dad7832239326f967e4
Shoksbe <deknyfgregory@gmail.com>
2018-11-06 22:02:10 +0100

    feat(*): adding customTableViewCell for ingredient

---
+++ Reciplease/View/IngredientTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  IngredientTableViewCell.swift
+//  Reciplease
+//
+//  Created by Gregory De knyf on 06/11/2018.
+//  Copyright © 2018 De knyf Gregory. All rights reserved.
+//
+
+import UIKit
+
+class IngredientTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ingredientName: UILabel!
+
+}
