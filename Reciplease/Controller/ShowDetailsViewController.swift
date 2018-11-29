0085e57aaa4961209c73edd5252dffbeac1013e0
Shoksbe <deknyfgregory@gmail.com>
2018-11-29 07:57:43 +0100

    feat(*): creating showDetailsController and GetRecipeDetailsService files

---
+++ Reciplease/Controller/ShowDetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ShowDetailsViewController.swift
+//  Reciplease
+//
+//  Created by Gregory De knyf on 22/11/2018.
+//  Copyright © 2018 De knyf Gregory. All rights reserved.
+//
+
+import UIKit
+
+class ShowDetailsViewController: UIViewController {
+    
+    var recipeToDetail: Recipe!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Get recipe's details
+
+    }
+
+}
---
+++ Reciplease/Model/GetRecipeDetailsService.swift
@@ -0,0 +1,14 @@
+//
+//  GetRecipeDetailsService.swift
+//  Reciplease
+//
+//  Created by Gregory De knyf on 29/11/2018.
+//  Copyright © 2018 De knyf Gregory. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class GetRecipeDetailsService {
+    
+}
