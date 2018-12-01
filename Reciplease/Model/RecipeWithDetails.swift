d195b696c84ed42f126cf8e5752a371f4647cd70
Shoksbe <deknyfgregory@gmail.com>
2018-12-01 10:37:36 +0100

    feat(): adding RecipeWithDetails file, that struct is used tho make a easy read object

---
+++ Reciplease/Model/RecipeWithDetails.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeWithDetails.swift
+//  Reciplease
+//
+//  Created by De knyf Gregory on 01/12/2018.
+//  Copyright © 2018 De knyf Gregory. All rights reserved.
+//
+
+import Foundation
+
+struct RecipeWithDetails {
+    let smallImageUrl: String?
+    let mediumImageUrl: String?
+    let largeImageUrl: String?
+    let name: String
+    let id: String
+    let ingredientLines: [String]
+    let totalTimeInSeconds: Int?
+    let rating: Int?
+}
