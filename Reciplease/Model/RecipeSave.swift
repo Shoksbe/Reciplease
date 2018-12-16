cd73f18460de38e2e8af830dadc345d5a65d8585
Greg De Knyf <deknyfgregory@gmail.com>
2018-12-16 20:48:31 +0100

    feat(CoreData): creating of the recipe element to save the recipe

---
+++ Reciplease/Model/RecipeSave.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeSave.swift
+//  Reciplease
+//
+//  Created by Gregory De knyf on 16/12/2018.
+//  Copyright © 2018 De knyf Gregory. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class RecipeSave: NSManagedObject {
+}
