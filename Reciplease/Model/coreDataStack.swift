0554662d881dc1448ffd0a1feb9b85e5c7655818
Shoksbe <deknyfgregory@gmail.com>
2019-01-22 10:20:16 +0100

    fix bug with coreData file

---
+++ Reciplease/Model/coreDataStack.swift
@@ -0,0 +1,72 @@
+//
+//  coreDataStack.swift
+//  Reciplease
+//
+//  Created by Gregory De knyf on 22/01/2019.
+//  Copyright © 2019 De knyf Gregory. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    private let modelName: String
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    lazy var mainContext: NSManagedObjectContext = {
+        return self.storeContainer.viewContext
+    }()
+    
+    lazy var storeContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    func newDerivedContext() -> NSManagedObjectContext {
+        let context = storeContainer.newBackgroundContext()
+        return context
+    }
+    
+    func saveContext() {
+        saveContext(mainContext)
+    }
+    
+    func saveContext(_ context: NSManagedObjectContext) {
+        if context != mainContext {
+            saveDerivedContext(context)
+            return
+        }
+        
+        context.perform {
+            do {
+                try context.save()
+            } catch let error as NSError {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+    
+    func saveDerivedContext(_ context: NSManagedObjectContext) {
+        context.perform {
+            do {
+                try context.save()
+            } catch let error as NSError {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+            
+            self.saveContext(self.mainContext)
+        }
+    }
+}
+
+
